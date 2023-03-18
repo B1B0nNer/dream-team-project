2ce1c81c534c91ae8403e1ea0b8cc57f03d87f0a
Velorian-TDV <terleckydima92@gmail.com>
2023-03-18 23:43:48 +0200

    subscribe block realise

--- src/scripts/subscribe.js
+++ src/scripts/subscribe.js
@@ -1,1 +1,17 @@
-console.log('work')
+(() => {
+    // Modal var obj
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+
+    // Modal events
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    // backdrop togle function
+    function toggleModal() {
+        refs.modal.classList.toggle("subscribe-backdrop-hiden");
+    }
+})();
