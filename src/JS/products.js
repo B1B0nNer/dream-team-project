a9b0c5f229981576086a0402cd626a26bb9ea1af
Ivankimachuk <123023827+Ivankimachuk@users.noreply.github.com>
2023-03-18 20:35:16 +0200

    test

--- src/JS/products.js
+++ src/JS/products.js
@@ -2,6 +2,28 @@ $(document).ready(function(){
 	$('.slider-list').slick({
         arrows:false,
         dots:true,
+        autoplay:true,
+        infinity:true,
+        autoplaySpeed:3000,
+        pauseOnFocus:true,
+        pauseOnHover:true,
+        pauseOnDotsHover:true,
+        variableWidth:true,
+        slidesToShow:3,
+        responsive:[
+            {
+                breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+            },{
+                breakpoint: 405,
+				settings: {
+					slidesToShow:1
+				}
+            }
+        ],
+        mobileFirst:true
     });
 		
 });
