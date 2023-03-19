b2f5ae2b6003074eac460b7399138c53fffbdf5b
Nikita <barbos.nik007@gmail.com>
2023-03-19 14:39:13 +0100

    Fix burger button

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -5,8 +5,9 @@
  
    const toggleMenu = () => {
      const isMenuOpen =
-       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+       openMenuBtn.getAttribute('aria-expanded') == true || false;
      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+     openMenuBtn.classList.toggle('is-open');
      mobileMenu.classList.toggle('is-open');
  
      const scrollLockMethod = !isMenuOpen
@@ -22,6 +23,7 @@
    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
      if (!e.matches) return;
      mobileMenu.classList.remove('is-open');
+     openMenuBtn.classList.remove('is-open');
      openMenuBtn.setAttribute('aria-expanded', false);
      bodyScrollLock.enableBodyScroll(document.body);
    });
