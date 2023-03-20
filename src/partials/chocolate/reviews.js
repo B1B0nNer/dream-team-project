55a471befe89d00332051ea48c1a557e284c0c7b
IhorIvankiv <igor.rocky@gmail.com>
2023-03-20 19:33:56 +0200

    fix

--- src/partials/chocolate/reviews.js
+++ src/partials/chocolate/reviews.js
@@ -4,21 +4,20 @@ const swiper = new Swiper('.swiper', {
 
   pagination: {
     el: '.swiper-pagination',
-    clickable: true
+    clickable: true,
   },
 
   breakpoints: {
     375: {
-        slidesPerView: 1,
-        spaceBetween: 20,
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
     768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
+      slidesPerView: 2,
+      spaceBetween: 30,
     },
     1200: {
-        slidesPerView: 3,
-    }
-}  
+      slidesPerView: 3,
+    },
+  },
 });
-
