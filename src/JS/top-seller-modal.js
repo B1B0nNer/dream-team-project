2f8422135e46f514afeba45ec6b05f489a55658a
Nikita <barbos.nik007@gmail.com>
2023-03-21 23:44:41 +0100

    ...

--- src/JS/top-seller-modal.js
+++ src/JS/top-seller-modal.js
@@ -1,14 +1,14 @@
 (() => {
-  const reffs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+    const refstop = {
+      openModalBtn: document.querySelector("[top-data-modal-open]"),
+      closeModalBtn: document.querySelector("[top-data-modal-close]"),
+      modal: document.querySelector("[top-data-modal]"),
+    };
+  
+    refstop.openModalBtn.addEventListener("click", toggleModal);
+    refstop.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refstop.modal.classList.toggle("is-hidden");
+    }
+  })();
